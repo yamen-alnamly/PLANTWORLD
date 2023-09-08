4620c2e86db8d20b44b5df3de6cc26b7ab71b08e
Yamen <125800880+yamen-alnamly@users.noreply.github.com>
2023-09-08 22:52:24 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+
+var body = document.getElementsByName(body);
+
+
+function backgroundChange() {
+    body.style.backgroundColor = 'red';
+}
+function backgroudnback(elememt) {
+    elememt.style.color = 'white';
+}
